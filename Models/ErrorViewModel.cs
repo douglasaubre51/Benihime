48116370cd69948005e354fbb5e96e49918d052e
Allen Bose <boseallen192@gmail.com>
2025-01-11 13:17:31 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Benihime.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
