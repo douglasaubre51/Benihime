6b6e0e36c26325b187320ab37355b62490269d43
Allen Bose <boseallen192@gmail.com>
2025-01-11 13:44:04 +0530

    created model address

---
+++ Models/Address.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Benihime.Models
+{
+    public class Address
+    {
+        [Key]
+        int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int State { get; set; }
+    }
+}
