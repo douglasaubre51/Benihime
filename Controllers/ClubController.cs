bab7046fbb09ac26415f6bb20a91d49dd2b07756
Allen Bose <boseallen192@gmail.com>
2025-01-14 22:19:54 +0530

    created controller and view

---
+++ Controllers/ClubController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Benihime.Controllers
+{
+    public class ClubController : Controller
+    {
+        // GET: ClubController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
