29e3756f0d4ecbaaaa67702fd9e8cfaeaef4d608
Allen Bose <boseallen192@gmail.com>
2025-01-18 09:22:47 +0530

    finished index view of Race

--- obj/Release/net9.0/Benihime.AssemblyInfo.cs
+++ obj/Release/net9.0/Benihime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Benihime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c950ab4b305c3a4f70a710b6346e7b1f47c7167e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db6a3ef3cf1fb9ef6c5c0c3126bfd94e3513e22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Benihime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Benihime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
