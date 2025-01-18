1896692aa31e7c9499eff584989cc2af2f6fb6b6
Allen Bose <boseallen192@gmail.com>
2025-01-18 19:50:12 +0530

    trying to ignore files 	modified:   obj/Release/net9.0/Benihime.AssemblyInfo.cs 	modified:   obj/Release/net9.0/Benihime.AssemblyInfoInputs.cache

--- obj/Release/net9.0/Benihime.AssemblyInfo.cs
+++ obj/Release/net9.0/Benihime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Benihime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d48d1c00abb7251abcd62c4a6b7363aa818a5900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf76d7b60096268193fcda2a4ef2bc7fe361a83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Benihime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Benihime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
