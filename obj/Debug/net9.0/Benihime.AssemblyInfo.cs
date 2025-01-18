5c1d5f42c2a5da92bdab9bce5ddd4ba86e8ceaab
Allen Bose <boseallen192@gmail.com>
2025-01-18 11:59:56 +0530

    metadata

--- obj/Debug/net9.0/Benihime.AssemblyInfo.cs
+++ obj/Debug/net9.0/Benihime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Benihime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b21a9e498812d008e0ce28236b8703ac1e2f8342")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c950ab4b305c3a4f70a710b6346e7b1f47c7167e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Benihime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Benihime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
